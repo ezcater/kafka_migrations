a02d232551b0fcd96d656082ec39087ac27dc0a3
Tim Perkins <tim.perkins@ezcater.com>
2018-02-21 17:45:03 -0500

    Configuration refactor

--- lib/kafka_migrations/configuration.rb
+++ lib/kafka_migrations/configuration.rb
@@ -1,40 +1,67 @@
+require "ostruct"
+
 module KafkaMigrations
   class Configuration
-    attr_writer :migrations_topic_name,
-                :seed_brokers,
-                :logger
-
-    #IVAR_REGEX = /^[a-z0-9_]+$/.freeze
-
-    # TODO: use openstruct as delegate
+    CONFIG_OPTIONS = {
+      migrations_topic_name: nil,
+      seed_brokers: nil,
+      logger: nil,
+      sasl_plain_username: nil,
+      sasl_plain_password: nil,
+      ssl_ca_cert_file_path: nil,
+      config_file: nil
+    }.freeze
 
     DEFAULTS = OpenStruct.new(num_partitions: 1,
                               replication_factor: 1,
                               topic_config: {}.freeze,
                               timeout: 30).freeze
 
-    def configure
-      yield self
+    attr_reader :delegate
+
+    def initialize
+      @delegate = OpenStruct.new(CONFIG_OPTIONS)
     end
 
-    def method_missing(name, *_args, &_blk)
-      instance_variable_get("@#{name}") ||
+    def configure
+      yield delegate
+    end
+
+    private
+
+    # We want to return nil instead of calling super for method_missing.
+    # rubocop:disable Style/MethodMissing
+    def method_missing(name, *args, &_blk)
+      if name.to_s.end_with?("=".freeze)
+        delegate.send(name, *args)
+      else
+        method_missing_reader(name)
+      end
+    end
+    # rubocop:enable Style/MethodMissing
+
+    def respond_to_missing?(name, _include_private = false)
+      if name.to_s.end_with?("=".freeze)
+        true
+      else
+        respond_to_missing_reader?(name) || super
+      end
+    end
+
+    def method_missing_reader(name)
+      delegate.public_send(name) ||
         rails_config_kafka_migrations_value(name) ||
         config_data_value(name) ||
         DEFAULTS.public_send(name) ||
         nil
     end
 
-    def respond_to_missing?(name, include_private = false)
-      #(name.match?(IVAR_REGEX) && instance_variable_defined?("@#{name}")) ||
-      methods.include?(:"#{name}=") ||
+    def respond_to_missing_reader?(name)
+      delegate.respond_to?(name) ||
         (defined?(Rails) && Rails.configuration.kafka_migrations.key?(name)) ||
         config_data.key?(name) ||
-        DEFAULTS.respond_to?(name) ||
-        super
+        DEFAULTS.respond_to?(name)
     end
-
-    private
 
     def config_data
       @config_data ||= load_config_data
@@ -52,14 +79,19 @@ module KafkaMigrations
     end
 
     def config_file_path
-      # TODO: allow this to be set from config first?
-      @config_file_path ||= defined?(Rails) && "#{Rails.application.root}/config/kafka.yml"
+      if defined?(@config_file_path)
+        @config_file_path
+      else
+        @config_file_path = delegate.config_file ||
+          rails_config_kafka_migrations_value("config_file".freeze) ||
+          (defined?(Rails) && "#{Rails.application.root}/config/kafka.yml")
+      end
     end
 
     def rails_config_kafka_migrations_value(name)
       defined?(Rails) &&
-      Rails.configuration.kafka_migrations.key?(name) &&
-        Rails.configuration.kafka_migrations.send(name)
+        Rails.configuration.kafka_migrations.key?(name) &&
+          Rails.configuration.kafka_migrations.send(name)
     end
   end
 end
